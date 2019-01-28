7c13a874fdb79accf24f84744cdc924447c0b08c
Csaba Szabó <szabcsabest@gmail.com>
2019-01-28 18:38:58 +0100

    universal get methods

--- data_manager.py
+++ data_manager.py
@@ -3,20 +3,16 @@ import connection
 
 # sample
 @connection.connection_handler
-def get_all_questions(cursor, first_attribute='', second_attribute=''):
-    attributes = ['submission_time-DESC', 'submission_time-ASC', 'view_number-DESC',
-                  'view_number-ASC', 'vote_number-DESC', 'vote_number-ASC', 'title-DESC', 'title-ASC', '']
-    if first_attribute in attributes and second_attribute in attributes:
-
-            SQL_part = 'ORDER BY' if first_attribute or second_attribute else ''
-            comma = ', ' if first_attribute and second_attribute else ''
-            first_attribute = first_attribute.replace('-', ' ')
-            second_attribute = second_attribute.replace('-', ' ')
-
-            cursor.execute(f"""
-                            SELECT * FROM question
-                            {SQL_part} {first_attribute}{comma}{second_attribute};
-                            """)
-            return cursor.fetchall()
+def get_boards(cursor, criteria):
+    if criteria:
+        cursor.execute(f"""
+                        SELECT * FROM boards
+                        WHERE {criteria['key']} = %(value)s
+                        """,
+                       criteria)
     else:
-        return 'That is not an option.'
+        cursor.execute(f"""
+                        SELECT * FROM boards
+                        """,
+                       )
+    return cursor.fetchall()
--- main.py
+++ main.py
@@ -1,12 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+import data_manager
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def boards():
+def index():
     ''' this is a one-pager which shows all the boards and cards '''
     return render_template('boards.html')
+
+
+@app.route('/boards', methods=['GET'])
+def get_boards():
+    criteria = request.args.to_dict()
+    return make_db_query(criteria, data_manager.get_boards)
+
+
+def make_db_query(criteria, getter_function):
+    if criteria:
+        key = list(criteria.keys())[0]
+        value = criteria[key]
+        criteria = {"key": key, "value": value}
+    try:
+        query_result = getter_function(criteria)
+    except:
+        return jsonify({"done": False, "reason": "Database error"})
+    else:
+        return jsonify({"done": True, "message": "Successful query", "result": query_result})
+
 
 
 def main():
