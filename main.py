f5f61e0c6d2bf8f04aecf83d16a57b243e6cd95f
Csaba Szabó <szabcsabest@gmail.com>
2019-01-29 15:17:22 +0100

    more RESTful routes

--- main.py
+++ main.py
@@ -21,27 +21,19 @@ def get_boards():
     return make_db_query(criteria, data_manager.get_boards)
 
 
-@app.route('/boards', methods=['DELETE'])
-def delete_board():
-    criteria = request.form.to_dict()
+@app.route('/boards/<_id>', methods=['DELETE'])
+def delete_board(_id):
+    criteria = {'id': _id}
     return delete_from_db(criteria, 'boards')
 
 
-@app.route('/cards', methods=['DELETE'])
-def delete_card():
-    criteria = request.form.to_dict()
+@app.route('/cards/<_id>', methods=['DELETE'])
+def delete_card(_id):
+    criteria = {'id': _id}
     return delete_from_db(criteria, 'cards')
 
 
 def delete_from_db(criteria, table):
-    if criteria:
-        key = list(criteria.keys())[0]
-        if key != 'id':
-            return jsonify({"done": False, "reason": f"Rows can be deleted from {table} only via id"})
-        value = criteria[key]
-        criteria = {"key": key, "value": value}
-    else:
-        return jsonify({"done": False, "reason": f"It is not specified, which row has to be deleted from {table}"})
     try:
         user_id_of_deleted_row = data_manager.delete_row(table, criteria)["user_id"]
     except:
