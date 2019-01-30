1383deae86ddfcc9e135ec6783529898be18a267
Tamas Pataky <tamas.pataky@outlook.com>
2019-01-30 13:57:09 +0100

    Drag and drop settings

--- static/js/main.js
+++ static/js/main.js
@@ -6,14 +6,68 @@ var app = new Vue({
       // draggable
     },
     data: {
+        list1:[],
+		list2:[{name:"Juan", id:5},
+				{name:"Edgard", id:6},
+        ],
+        newBoard: null,
+        newColumn: null,
         drag: false,
-        boards: sampleData.boards,
-        columns: ['El sem indult', 'Kicsit késik', 'Sokat késik', 'Eltűnt']
+        // boards: sampleData.boards,
+
+        // columns: ['El sem indult', 'Kicsit késik', 'Sokat késik', 'Eltűnt'],
+        boards: [
+            { title: 'MÁV' , columns: [
+                {title: 'El sem indult', cards: [
+                    {title:"John", id:1},
+                    {title:"Joao", id:2},
+                    {title:"Jean", id:3},
+                    {title:"Gerard", id:4} ]},
+                {title: 'Kicsit késik', cards: [
+                    {title:"Juan", id:5},
+				    {title:"Edgard", id:6},
+                    ]},
+
+                ]
+            }
+        ],
+        columns: [
+            {title: 'El sem indult', cards: ['Thomas', 'Rosie', 'Edward', 'Henry', 'Gordon', 'James']},
+        ],
+        // cards: ['Thomas', 'Rosie', 'Edward', 'Henry', 'Gordon', 'James']
     },
     methods: {
+        closeModalWarning() {
+            console.log('close')
+            $('#modalWarning').modal('hide')
+            this.newBoard = null
+        },
         addBoard() {
-            this.boards.push({ title: 'Board ' + (this.boards.length + 1), id:this.boards.length + 1})
+            if (this.newBoard === 'Arrived on time' || this.newBoard === 'Időben érkezett' ) {
+               $('#modalWarning').modal('show')
+               return
+            } else if (this.newBoard) {
+                this.boards.unshift({ title: this.newBoard, id:this.boards.length + 1, columns:[]})
+                this.newBoard = null
+            }
+
+        },
+        addColumn(board) {
+            if (this.newColumn) {
+                board.columns.unshift({ title: this.newColumn, id:board.length + 1, cards:[]})
+                this.newColumn = null
+            }
         }
+    },
+    async mounted () {
+        const data = await fetch('/boards')  // set the path; the method is GET by default, but can be modified with a second parameter
+        .then((response) => response.json())
+        // this.boards = data.result
+
+        /*// parse JSON format into JS object
+        .then((data) => {
+            this.boards = data.result;
+        })*/
     }
 })
 
